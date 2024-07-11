80e4520ace14a66f7afec7c8311f97107bf38bcd
HannahFromAland <171098175@smail.nju.edu.cn>
2024-07-10 22:56:54 -0700

    Half of lec 2 commit tapOnGesture, ForEach

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["🙂‍↔️", "🥰", "😇", "🙂‍↔️"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            // Cannot do for loop here for emoji assignment
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .padding()
+        .foregroundColor(.mint)
+    }
+}
+
+struct CardView: View {
+    let content: String
+    // @State will create a pointer for isFaceUp which will never change, so the content it is pointing to can change
+    @State var isFaceUp = true
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            // views are immutable
+            // isFaceUp = !isFaceUp
+            isFaceUp.toggle()
+        }
     }
 }
 
